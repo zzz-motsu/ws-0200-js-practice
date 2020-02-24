73cedad4bfeac884da9f7d385dd185670421d06e
version-1 <j.onodera26@gmail.com>
2020-02-24 09:12:30 -0800

    add question

--- 001_syntax/005_array.js
+++ 001_syntax/005_array.js
@@ -35,3 +35,43 @@ function getDays() {
     'Sunday',
   ];
 }
+
+/**
+ *  5.3 配列に特定の数があった場合にtrue、そうでない場合にfalseを返す関数を実装してください
+ *
+ *  example:
+ *    [1, 2, 3], 1 => true
+ *    [1, 2, 3], 5 => false
+ *    [], 5 => false
+ *
+ */
+
+function findNum(array, num) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ *  5.4 配列に重複した要素があれば、true、そうでなければfalseを返す関数を実装してください
+ *
+ *  example:
+ *    [1, 2, 3], 1 => false
+ *    [1, 2, 2, 3], 5 => true
+ *    [] => false
+ *
+ */
+
+function isDuplicate(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j && array[i] === array[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
