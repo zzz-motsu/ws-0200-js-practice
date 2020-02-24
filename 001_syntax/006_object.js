69184cad1bc64029ee12c9512a643beae5eb78ba
version-1 <j.onodera26@gmail.com>
2020-02-24 09:33:35 -0800

    add question

--- 001_syntax/006_object.js
+++ 001_syntax/006_object.js
@@ -1,8 +1,89 @@
 /**
- *  6.1
+ *  6.1 下記データを持ったオブジェクトを返す関数を実装してください。
+ *
+ *  name: 'Bob'
+ *  age: 32
+ *  gender: 'male'
  *
  */
 
-function () {
+function getPersonObject() {
+  return {
+    name: 'Bob',
+    age: 32,
+    gender: 'male',
+  };
 }
 
+/**
+ *  6.2 下記データAが引数で与えられた場合にデータBに書き換える関数を実装してください。
+ *
+ *  Data A:
+ *    name: 'Bob'
+ *    age: 32
+ *    gender: 'male'
+ *  Data B:
+ *    name: 'Mary'
+ *    age: 37
+ *    gender: 'female'
+ *
+ */
+
+function mutateObject(person) {
+  person.name = 'Mary';
+  person.age = 37;
+  person.gender = 'female';
+  return person;
+}
+
+/**
+ *  6.3 下記引数で渡される配列にランダムな1 ~10の数字を割り振り、オブジェクトとして返す
+ *      関数を実装してください
+ *
+ *    [
+ *      'Bob',
+ *      'Mary',
+ *      'Ann',
+ *      'Mike'
+ *    ]
+ *
+ *   output:
+ *     {
+ *       Bob: [Random Number],
+ *       Mary: [Random Number],
+ *       Ann: [Random Number],
+ *       Mike: [Random Number]
+ *     }
+ *
+ */
+
+function assignNumber(persons) {
+  let obj = {};
+  for (let i = 0; i < persons.length; i++) {
+    const rand = Math.floor(Math.random() * 11);
+    obj[persons[i]] = rand;
+  }
+  return obje;
+}
+
+/**
+ *  6.4 配列に重複した要素があれば、true、そうでなければfalseを返す関数を実装してください
+ *      但し、オブジェクトを使って実装すること
+ *
+ *  example:
+ *    [1, 2, 3], 1 => false
+ *    [1, 2, 2, 3], 5 => true
+ *    [] => false
+ *
+ */
+
+function isDuplicate(array) {
+  numMap = {}
+  for (let i = 0; i < array.length; i++) {
+    if (numMap[array[i]]) {
+       return true
+    }
+    numMap[array[i]] = true
+  }
+  return false;
+}
