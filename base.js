e65d4f1633d67d8dc4b7d7497f2cf5c2e817e1c4
version-1 <j.onodera26@gmail.com>
2020-02-23 23:27:11 -0800

    add files

---
+++ base.js
@@ -0,0 +1,85 @@
+
+/**
+ *  1.1 Hello Worldという文字を出力するメソッドを定義してください
+ *
+ */
+function helloWorld() {
+  console.log("Hello World")
+}
+
+/**
+ *  1.2 const を使って定義した変数を出力するdisplayConstメソッドを実装してください
+ *
+ */
+function displayConst() {
+  const hoge = 'hoge'
+  console.log(hoge)
+}
+
+/**
+ *  1.3 let を使って定義した変数を出力するdisplayLetメソッドを実装してください
+ *
+ */
+function displayLet() {
+  let hoge = 'hoge'
+  console.log(hoge)
+}
+
+/**
+ *  1.4 引数に渡した変数を表示するメソッドを実装してください
+ *
+ */
+function displayArgument(arg) {
+  console.log(arg)
+}
+
+/**
+ *  1.5 渡された二つの引数の合計を返却するメソッドを実装してください
+ *
+ */
+function sumTwoArgs(a, b) {
+  return a + b
+}
+
+/**
+ *  1.6 渡された二つの引数の差を返却するメソッドを実装してください
+ *
+ */
+function subtractTwoArgs(a, b) {
+  return a - b
+}
+
+/**
+ *  1.7 渡された二つの引数の商を返却するメソッドを実装してください
+ *
+ */
+function quotientTwoArgs(a, b) {
+  if (b === 0) {
+    return null
+  }
+  return a / b
+}
+
+/**
+ *  1.8 渡された二つの引数の積を返却するメソッドを実装してください
+ *
+ */
+function productTwoArgs(a, b) {
+  return a * b
+
+/**
+ *  1.9 渡された引数の2で割ったあまりを返却するメソッドを実装してください
+ *
+ */
+function remainderTwoArgs(a) {
+  return a % 2
+}
+
+/**
+ *  1.10 渡された引数が2の倍数であることを確認するメソッドを実装してください
+ *
+ */
+function isEven(a) {
+  return a % 2 === 0
+}
+
