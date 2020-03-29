c2dad9e65494b1eafe7bcf8ef4b626e157a9e0ff
version-1 <j.onodera26@gmail.com>
2020-03-29 14:23:37 -0700

    add questions

---
+++ 002_algorithm/003_search.js
@@ -0,0 +1,52 @@
+
+/**
+ *  2.3.1 リニアサーチ
+ *
+ *  リニアサーチを実装してください。(入力は数値のみ)
+ *  二つ目の引数に合致した配列の添字を返却してください。
+ *  合致のする数字のない場合は-1を返却してください。
+ *
+ *  example:
+ *    [1, 3, 2, 4, 5], 3 => 1
+ *    [5, 3, 2, 1], 6 => -1
+ */
+
+function linearSearch (array, target) {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] === target) {
+      return i
+    }
+  }
+  return -1
+}
+
+/**
+ *  2.3.2 バイナリサーチ
+ *
+ *  バイナリサーチを実装してください。(入力は数値のみ)
+ *
+ *  example:
+ *    [1, 2, 3, 4, 5], 2 => 2
+ *    [1, 2, 3, 4] 5 => -1
+ */
+
+function binarySearch (array, target) {
+  let left = 0
+  let right = array.length - 1
+
+  while(left <= right) {
+    mid = Math.floor((left + right) / 2)
+    if (array[mid] === target) {
+      return mid
+    }
+
+    if (array[mid] < target) {
+      left = mid + 1
+    } else {
+      right = mid - 1
+    }
+
+  }
+  return -1
+}
+
---
+++ 002_algorithm/004_recursive.js
@@ -0,0 +1,111 @@
+
+/**
+ *  2.4.1 数列の和
+ *
+ *  再帰処理を用いて、0からnまでの数列の和を返す関数を実装してください
+ *
+ *  example:
+ *    3 => 6
+ *    10 => 55
+ *    30 => 55
+ */
+
+function sumSequence (n, sum = 0) {
+  if (n === 0) {
+    return sum
+  }
+
+  return sumSequence(n - 1, sum + n)
+}
+
+/**
+ *  2.4.2 フィボナッチ数
+ *
+ *  指定された数のフィボナッチ数を返却する関数を実装してください。
+ *  https://www.studyplus.jp/445
+ *
+ *  example:
+ *    input: 10 => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+ */
+
+function fibonacci (num, index = 0, array = []) {
+  if (num == 0) {
+    return array
+  }
+
+  if (index === 0 || index === 1) {
+    array.push(1)
+    return fibonacci(num - 1, index + 1, array)
+  }
+
+  array.push(array[index - 2] + array[index - 1])
+  return fibonacci(num - 1, index + 1, array)
+}
+
+
+/**
+ *  2.4.2 ディレクトリに含まれるファイルサイズの合計
+ *
+ *  ツリー上のオブジェクトで渡されるディレクトリの
+ *  ファイルサイズの合計を求める関数を実装してください。
+ *
+ *  example:
+ *    {
+ *      type: 'folder',
+ *      size: 0
+ *      children: [
+ *        {
+ *          type: 'folder',
+ *          size: 0
+ *          children: [
+ *            {
+ *              type: 'folder'
+ *              size: 0
+ *              children: [
+ *                {
+ *                  type: 'file',
+ *                  size: 5
+ *                },
+ *                {
+ *                  type: 'file',
+ *                  size: 7
+ *                },
+ *                {
+ *                  type: 'file',
+ *                  size: 9
+ *                },
+ *              ]
+ *            }
+ *          ]
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 3
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 4
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 10
+ *        },
+ *      ]
+ *    }
+ *    => 38
+ */
+
+function fileSize (node, sum = 0) {
+  if (!node.children) {
+    return sum + node.size
+  }
+
+  return node.children.reduce((acc, item) => {
+    if (item.children) {
+      return acc + fileSize(item)
+    }
+    return acc + item.size
+  }, sum)
+}
+
+
