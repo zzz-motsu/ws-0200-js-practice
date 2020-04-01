8c306a1487bad44d9c0e05a33f59a186658b65a8
version-1 <j.onodera26@gmail.com>
2020-04-01 12:11:06 -0700

    add question

---
+++ 002_algorithm/004_recursive.js
@@ -0,0 +1,88 @@
+
+/**
+ *  2.4.1 数列の和
+ *
+ *  再帰処理を用いて、0からnまでの数列の和を返す関数を実装してください
+ *
+ *  example:
+ *    3 => 6
+ *    10 => 55
+ *    30 => 55
+ */
+
+function sumSequence (n, sum = 0) {
+  return n
+}
+
+/**
+ *  2.4.2 フィボナッチ数
+ *
+ *  指定された数のフィボナッチ数を返却する関数を実装してください。
+ *  https://www.studyplus.jp/445
+ *
+ *  example:
+ *    input: 10 => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+ */
+
+function fibonacci (num, index = 0, array = []) {
+  return num
+}
+
+
+/**
+ *  2.4.2 ディレクトリに含まれるファイルサイズの合計
+ *
+ *  ツリー上のオブジェクトで渡されるディレクトリの
+ *  ファイルサイズの合計を求める関数を実装してください。
+ *
+ *  example:
+ *    {
+ *      type: 'folder',
+ *      size: 0
+ *      children: [
+ *        {
+ *          type: 'folder',
+ *          size: 0
+ *          children: [
+ *            {
+ *              type: 'folder'
+ *              size: 0
+ *              children: [
+ *                {
+ *                  type: 'file',
+ *                  size: 5
+ *                },
+ *                {
+ *                  type: 'file',
+ *                  size: 7
+ *                },
+ *                {
+ *                  type: 'file',
+ *                  size: 9
+ *                },
+ *              ]
+ *            }
+ *          ]
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 3
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 4
+ *        },
+ *        {
+ *          type: 'file',
+ *          size: 10
+ *        },
+ *      ]
+ *    }
+ *    => 38
+ */
+
+function fileSize (node, sum = 0) {
+  return sum
+}
+
+
