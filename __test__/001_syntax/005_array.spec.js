e404880cd7564697ef036668f5f442d9ba670487
version-1 <j.onodera26@gmail.com>
2020-07-25 01:17:15 -0700

    add test for 005

--- __test__/001_syntax/005_array.spec.js
+++ __test__/001_syntax/005_array.spec.js
@@ -1,102 +1,66 @@
 const { logMock } = require('../index');
 const {
-  printOneToTen,
-  printOneToTenDesc,
-  printOneToTenWhile,
-  printOneToTenWhileDesc,
-} = require('../../001_syntax/003_loop');
+  printArray,
+  getDays,
+  findNum,
+  isDuplicate,
+} = require('../../001_syntax/005_array');
 
 const origin = console.log;
 afterEach(() => (console.log = origin));
 
-describe('printOneToTen', () => {
+describe('printArray', () => {
   let outputs = [];
   beforeEach(() => {
     outputs = []
     console.log = logMock(outputs)
   });
   test('正常系', () => {
-    printOneToTen();
+    printArray([1, 2, 3, 4, 5]);
 
-    expect(outputs[0]).toEqual(0);
-    expect(outputs[1]).toEqual(1);
-    expect(outputs[2]).toEqual(2);
-    expect(outputs[3]).toEqual(3);
-    expect(outputs[4]).toEqual(4);
-    expect(outputs[5]).toEqual(5);
-    expect(outputs[6]).toEqual(6);
-    expect(outputs[7]).toEqual(7);
-    expect(outputs[8]).toEqual(8);
-    expect(outputs[9]).toEqual(9);
-    expect(outputs[10]).toEqual(10);
+    expect(outputs[0]).toEqual(1);
+    expect(outputs[1]).toEqual(2);
+    expect(outputs[2]).toEqual(3);
+    expect(outputs[3]).toEqual(4);
+    expect(outputs[4]).toEqual(5);
+  });
+
+  test('empty', () => {
+    printArray([]);
+
+    expect(outputs.length).toEqual(0);
   });
 });
 
-describe('printOneToTenDesc', () => {
-  let outputs = [];
-  beforeEach(() => {
-    outputs = []
-    console.log = logMock(outputs)
-  });
+describe('getDays', () => {
   test('正常系', () => {
-    printOneToTenDesc();
+    const days = getDays();
 
-    expect(outputs[0]).toEqual(10);
-    expect(outputs[1]).toEqual(9);
-    expect(outputs[2]).toEqual(8);
-    expect(outputs[3]).toEqual(7);
-    expect(outputs[4]).toEqual(6);
-    expect(outputs[5]).toEqual(5);
-    expect(outputs[6]).toEqual(4);
-    expect(outputs[7]).toEqual(3);
-    expect(outputs[8]).toEqual(2);
-    expect(outputs[9]).toEqual(1);
-    expect(outputs[10]).toEqual(0);
+    expect(days[0]).toEqual('Monday');
+    expect(days[1]).toEqual('Tuesday');
+    expect(days[2]).toEqual('Wednesday');
+    expect(days[3]).toEqual('Thursday');
+    expect(days[4]).toEqual('Friday');
+    expect(days[5]).toEqual('Saturday');
+    expect(days[6]).toEqual('Sunday');
   });
 });
 
-describe('printOneToTenWhile', () => {
-  let outputs = [];
-  beforeEach(() => {
-    outputs = []
-    console.log = logMock(outputs)
-  });
+describe('findNum', () => {
   test('正常系', () => {
-    printOneToTenWhile();
-
-    expect(outputs[0]).toEqual(0);
-    expect(outputs[1]).toEqual(1);
-    expect(outputs[2]).toEqual(2);
-    expect(outputs[3]).toEqual(3);
-    expect(outputs[4]).toEqual(4);
-    expect(outputs[5]).toEqual(5);
-    expect(outputs[6]).toEqual(6);
-    expect(outputs[7]).toEqual(7);
-    expect(outputs[8]).toEqual(8);
-    expect(outputs[9]).toEqual(9);
-    expect(outputs[10]).toEqual(10);
+    expect(findNum([1, 2, 3], 1)).toEqual(true);
+    expect(findNum([1, 2, 3], 3)).toEqual(true);
+    expect(findNum([1, 2, 3], 4)).toEqual(false);
+    expect(findNum([], 1)).toEqual(false);
   });
 });
 
-describe('printOneToTenWhileDesc', () => {
-  let outputs = [];
-  beforeEach(() => {
-    outputs = []
-    console.log = logMock(outputs)
-  });
+describe('isDuplicate', () => {
   test('正常系', () => {
-    printOneToTenWhileDesc();
-
-    expect(outputs[0]).toEqual(10);
-    expect(outputs[1]).toEqual(9);
-    expect(outputs[2]).toEqual(8);
-    expect(outputs[3]).toEqual(7);
-    expect(outputs[4]).toEqual(6);
-    expect(outputs[5]).toEqual(5);
-    expect(outputs[6]).toEqual(4);
-    expect(outputs[7]).toEqual(3);
-    expect(outputs[8]).toEqual(2);
-    expect(outputs[9]).toEqual(1);
-    expect(outputs[10]).toEqual(0);
+    expect(isDuplicate([1, 2, 3, 2])).toEqual(true);
+    expect(isDuplicate([2, 2, 2, 2])).toEqual(true);
+    expect(isDuplicate([1, 2, 2, 3])).toEqual(true);
+    expect(isDuplicate([1, 2, 3])).toEqual(false);
+    expect(isDuplicate([])).toEqual(false);
   });
 });
